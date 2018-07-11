59eeb29dc5ff73cf75a69d2fc438931f9d5bb58d
Robbie <583198238@qq.com>
2018-07-11 21:06:21 +0800

    test

--- target/classes/Test2.java
+++ target/classes/Test2.java
@@ -3,6 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("maven test edit on Github");
 		System.out.println("maven test");
 		System.out.println("maven test2");
 	}
