2345072ca4758f35a9b40523c30c75cd969c0854
qwer1234yy <33076669+qwer1234yy@users.noreply.github.com>
2018-07-11 21:10:58 +0800

    tet
    test

--- src/main/resources/Test2.java
+++ src/main/resources/Test2.java
@@ -6,6 +6,7 @@ public class Test2 {
 		System.out.println("maven test edit on Github");
 		System.out.println("maven test");
 		System.out.println("maven test2");
+		System.out.println("maven test3");
 	}
 
 }
